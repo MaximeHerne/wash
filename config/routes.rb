8c95b228f2c01aca68b45b6fed749935e8195d4c
MaximeHerne <herne.maxime@gmail.com>
2014-12-11 12:56:03 +0100

    order forms beginning

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
   root to: "pages#home"
 
-  resources :order do
+  resources :orders do
     resources :review , only: [:show, :edit, :update, :new, :create]
   end
   resources :profiles, only: [:show, :edit, :update, :new, :create]
