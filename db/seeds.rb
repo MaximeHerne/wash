374cdf221ffcb3f525bc7ae99a3d3152fbad9517
Florian Boret <florianboret@gmail.com>
2014-12-15 13:45:50 +0100

    profile setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,22 @@
 Formula.destroy_all
 Formula.create(name: 'Sans lessive', price: 5)
 Formula.create(name: 'Avec lessive', price: 9)
+
+
+10.times do
+  user = User.new({
+    email: Faker::Internet.email,
+    password: Faker::Internet.password,
+  })
+
+  user.profile = Profile.create({
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    phone: Faker::PhoneNumber.phone_number,
+    address: Faker::Address.street_name,
+    locality: Faker::Address.city,
+    postal_code: Faker::Address.zip_code,
+    })
+
+  user.save
+end
