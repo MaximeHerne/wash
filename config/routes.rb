87d54a06331f23382e7a7d75a30a056afd5feb0d
MaximeHerne <herne.maxime@gmail.com>
2014-12-22 11:35:57 +0100

    navbar jquery + static pages

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,11 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
   root to: "pages#home"
 
-
   resources :orders do
     resources :review , only: [:show, :edit, :update, :new, :create]
   end
   resources :profiles, only: [:show, :edit, :update, :new, :create]
 
+  get "pages/faq", :as => 'faq_page'
+  get "pages/contact", :as => 'contact_page'
 end
