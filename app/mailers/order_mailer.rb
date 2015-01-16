b321d786218a7e8e485d299f75ee6554292c651c
Florian Boret <florianboret@gmail.com>
2015-01-16 15:52:48 +0100

    fixed mail

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -4,7 +4,7 @@ class OrderMailer < ActionMailer::Base
   def new_order(order)
     @order = order
     mail( to: @order.user.email,
-          bcc: "contact@washed.fr", "florianboret@gmail.com",
+          bcc: "contact@washed.fr" ,
           subject: 'Nouvelle commande confirmée')
   end
 end
